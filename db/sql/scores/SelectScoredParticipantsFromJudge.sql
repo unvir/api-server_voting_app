9a75ab58d2d2f58cfd4614a1b8c3f8979ae6cb9f
unknown <unvir@yandex.ru>
2020-01-12 18:07:53 +0300

    Update judge select

--- db/sql/scores/SelectScoredParticipantsFromJudge.sql
+++ db/sql/scores/SelectScoredParticipantsFromJudge.sql
@@ -1,4 +1,4 @@
-SELECT scores."participantId", scores."value"
+SELECT scores."participantId", scores."value", scores."eventId", scores."fromUserId" as "judgeId"
 FROM public."Scores" AS scores
 WHERE scores."eventId" = ${eventId}
     AND scores."fromUserId" = ${judgeId}
