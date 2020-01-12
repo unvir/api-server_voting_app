a8f513d8fbbdbe0de8471a9c985918665f27f338
unknown <unvir@yandex.ru>
2020-01-12 14:19:26 +0300

    Fixed query for scored participants

--- db/sql/scores/SelectScoredParticipantsFromJudge.sql
+++ db/sql/scores/SelectScoredParticipantsFromJudge.sql
@@ -1,9 +1,4 @@
-SELECT participants."id" AS "participantId",
-  participants."fullName",
-  SUM(scores."value") AS "totalScore"
+SELECT scores."participantId", scores."value"
 FROM public."Scores" AS scores
-INNER JOIN public."Participants" AS participants
-    ON participants."id" = scores."participantId"
 WHERE scores."eventId" = ${eventId}
     AND scores."fromUserId" = ${judgeId}
-GROUP BY participants."id"
